557d8e9e33c25a5a736a08311e8c2b96bcbcbedc
Moi Petitnoel <moivp4@gmail.com>
2020-02-12 17:35:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,13 @@
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = 
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
 end
+
+def link
+  puts 
